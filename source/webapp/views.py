6556324bc197b5eeced3075d531221fb9fd86119
464716 <diesel.kg@mail.ru>
2019-09-25 17:20:56 +0600

    Приступил у вьюхе

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from webapp.models import Todo
 
+
+def todo_index(request, *args, **kwargs):
+    todos = Todo.objects.all()
+    return render(request, 'todo_index.html', context={
+        'todos': todos,
+    })
 # Create your views here.
