2d0e57ad72922661b8464ceb27dcf459d6a8a19c
464716 <diesel.kg@mail.ru>
2019-10-09 20:51:44 +0600

    Update, Delete views for type

--- source/webapp/views/type_views.py
+++ source/webapp/views/type_views.py
@@ -1,9 +1,9 @@
 from webapp.models import TypeChoice
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import View, ListView, CreateView
+from django.views.generic import ListView, CreateView,  UpdateView, DeleteView
 from webapp.forms import TypeForm
-from django.urls import reverse
-from .base import UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+# from .base import UpdateView, DeleteView
 
 
 class TypesView(ListView):
@@ -13,27 +13,46 @@ class TypesView(ListView):
 
 
 class TypeUpdateView(UpdateView):
-    form_class = TypeForm
-    template_name = 'types/type_update.html'
-    # redirect_url = 'todos/todo_view.html'
     model = TypeChoice
-    pk_url_kwarg = 'pk'
+    template_name = 'types/type_update.html'
     context_object_name = 'type'
+    form_class = TypeForm
 
-    def get_redirect_url(self):
+    def get_success_url(self):
         return reverse('type')
+
+
+# class TypeUpdateView(UpdateView):
+#     form_class = TypeForm
+#     template_name = 'types/type_update.html'
+#     # redirect_url = 'todos/todo_view.html'
+#     model = TypeChoice
+#     pk_url_kwarg = 'pk'
+#     context_object_name = 'type'
+#
+#     def get_redirect_url(self):
+#         return reverse('type')
 
 
 class TypeDeleteView(DeleteView):
-    template_name = 'types/type_delete.html'
-    # redirect_url = 'todos/todo_view.html'
     model = TypeChoice
-    pk_url_kwarg = 'pk'
+    template_name = 'types/type_delete.html'
+    context_key = 'type'
+    redirect_url = reverse_lazy('type')
     context_object_name = 'type'
-    confirm_delete = True
+    success_url = reverse_lazy('type')
 
-    def get_redirect_url(self):
-        return reverse('type')
+
+# class TypeDeleteView(DeleteView):
+#     template_name = 'types/type_delete.html'
+#     # redirect_url = 'todos/todo_view.html'
+#     model = TypeChoice
+#     pk_url_kwarg = 'pk'
+#     context_object_name = 'type'
+#     confirm_delete = True
+#
+#     def get_redirect_url(self):
+#         return reverse('type')
 
 
 class TypeCreateView(CreateView):
